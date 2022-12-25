66c7bfcbb363e3175f1db0e6b22a4ee7db73b7ff
animesh singh <animeshsingrol@gmail.com>
2022-12-25 10:48:21 +0530

    statementprint

---
+++ statementproblem.js
@@ -0,0 +1,57 @@
+//statement problem1
+
+let b="Masai School";
+console.log(b);
+
+let a="A transformation in Education"
+console.log(a);
+
+//statement problem2
+
+let name="Aniemsh Singh"
+console.log(name)
+
+name="Ajai Singh"
+console.log(name)
+
+name="Asha Singh"
+console.log(name)
+
+//statement problem3
+
+let name="Animesh Singh";
+let age==24;
+
+console.log(name,age);
+console.log(typeof(name),typeof(age))
+
+//statement problem4result
+
+onsole.log(`
+                    ██████████████████████████████████████
+                    █▄─▄▄▀█▄─▄▄─█─▄▄▄▄█▄─██─▄█▄─▄███─▄─▄─█
+                    ██─▄─▄██─▄█▀█▄▄▄▄─██─██─███─██▀███─███
+                    ▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▄▀▀▄▄▄▀▀`)
+        let x="Name:";
+        let name="Ravi Shastri";
+        let y="School:"
+        let school="shps";
+        let grade=10;
+        let z="section:";
+        let section="A";
+        let a="rollno.:";
+        let rollno=24;
+        let b="Mathematics:";
+        let marks_sub1=24;
+        let c="Chemistry:";
+        let marks_sub2=32;
+        let d="Physics:";
+        let marks_sub3=45;
+      console.log(x,       name);
+      console.log(y,    school);
+      console.log(z,  section);
+      console.log(a,  rollno);
+      console.log(b,marks_sub1);
+      console.log(c,marks_sub2);
+      console.log(d,marks_sub3);
+
