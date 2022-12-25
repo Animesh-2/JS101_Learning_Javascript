341c450df4fe1239fddf0621e8f5cb4c218a8f4c
animesh singh <animeshsingrol@gmail.com>
2022-12-25 11:10:58 +0530

    statementproblem

---
+++ problemstat.js
@@ -0,0 +1,32 @@
+//statement problem1
+
+let b="Masai School";
+console.log(b);
+let a="A transformation in Education"
+console.log(a);
+
+//statement problem2
+
+let name="Aniemsh Singh"
+console.log(name)
+name="Ajai Singh"
+console.log(name)
+name="Asha Singh"
+console.log(name)
+
+
+//statement problem3
+
+
+let name="Animesh Singh";
+let age=24;
+console.log(name,age);
+console.log(typeof(name),typeof(age))
+
+
+//statement problem4
+
+let name="Animesh Singh";
+let age=24;
+console.log(name,age);
+console.log(typeof(name),typeof(age))
